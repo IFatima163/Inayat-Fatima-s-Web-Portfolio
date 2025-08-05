3b60c214fa67f2bd51c7d15ca85b0252891f3e1e
Inayat Fatima Qureshi <inayat.fatima.qureshi@gmail.com>
2025-08-05 23:09:30 +0800

    Fixed base path for github pages

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/Inayat-Fatima-s-Web-Portfolio",
+  base: "/Inayat-Fatima-s-Web-Portfolio/",
 })
